4a5582f807fa90bb1cb04a57ae9245b75441c9f5
marksheekey <marksheekey@hotmail.com>
2020-11-30 15:14:54 +0000

    Init

---
+++ app/src/main/java/co/uk/happyapper/lushspacex/MainActivity.kt
@@ -0,0 +1,18 @@
+package co.uk.happyapper.lushspacex
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import co.uk.happyapper.lushspacex.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
